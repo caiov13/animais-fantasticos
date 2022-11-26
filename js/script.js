a8fd45c48bdd39243c60aaaaa06b3d7644703d4a
Caio Vinicius <caiov3d@outlook.com>
2022-11-26 19:22:51 -0300

    modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initAnimationScroll from "./modules/scroll-animation.js";
 import initSmoothScroll from "./modules/scoll-smooth.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
 
 initAnimationScroll();
 initSmoothScroll();
 initAccordion();
 initTabNav();
+initModal();
